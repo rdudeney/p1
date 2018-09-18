5cbffa9feab4c3eb7c4d29c5d01e1f2b0eff7120
Rupert <rupert.dudeney@gmail.com>
2018-09-18 02:14:47 -0400

    Added homepage and stylesheet

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Rupert Dudeney</title>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+	<div class="container">
+
+		<h1>Rupert Dudeney</h1>
+
+		<img src='images/09182018headshot.jpg' alt='Rupert Dudeney'>
+
+		<h2>About Me</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+		</p>
+
+		<h2>Random Quote</h2>
+		<blockquote>
+			Do your own thing on your own terms and get what you came here for. -Oliver James		</blockquote>
+
+	</div>
+
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
